c98edb4ed72c44704268b416e4553f09d8daa9d0
eikonikos <npanag_2@yahoo.com>
2022-07-11 02:45:03 +0300

    changes in files dokimi_test1.py and for_test1.py
    Some changes happened in 2 files in order to check different commands

---
+++ dokimi_test1.py
@@ -0,0 +1,20 @@
+x=0
+a=10
+b=-5
+
+j=0
+if a>0:
+    if b>0:
+        x+=5
+    elif a<5 or b<-2:
+        x+=3
+    else:
+        x+=3
+else:
+    x+=4
+print(x)
+
+for j in range(0,20):
+    print("Kalhspera")
+    if j==10:
+        break
